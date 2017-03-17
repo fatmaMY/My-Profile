0d1510614b40775042bec1dba6d87dd82ca55ab8
fatmaMY <fatima.mohammad736@gmail.com>
2017-03-17 21:42:45 +0300

    trying to get some data from ajax request but it's not working

--- js/project.js
+++ js/project.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
-	makeAjaxReqFav()
+	onClickFav();
+	onClickCurrent();
 	});
  
- function makeAjaxReqFav(e) {
- 	var fav=$( "#result" ).load( "https://github.com/fatmaMY.html a.text-bold")
- 	fav.addHeader("Access-Control-Allow-Origin", "https://github.com/fatmaMY");
-
+ function makeAjaxReqFav() {
+ 	//var fav=$( "#result" ).load( "https://github.com/fatmaMY.html a.text-bold")
+ 	// .fail(function(error){
+		// 	$('#imgLoading').fadeOut();
+		// 	$('#error').fadeIn();
+		// 	});
+ 	$.ajax({
+		url: 'https://github.com/fatmaMY',
+		type: 'get',
+		dataType: 'html',
+		headers: {
+			Authorization: "token "+"76c016f0e7fdd5dc25dfc7d89efdf0452c067b6d"
+		},
+		 success: function(data){
+      $('div.pinned-repo-item-content').html($(data).find('div.pinned-repo-item-content span a.text-bold').html());
+   }
+		
+		
+	 }).fail(function(error){
+	 	$('#imgLoading').fadeOut();
+		 	$('#error').fadeIn();
+ 	});
  }
+ 
 
  function onClickFav(e) {
  	$('#fav').click(function(e){
@@ -16,6 +36,22 @@ $(document).ready(function(){
 		e.preventDefault();
 		makeAjaxReqFav();
 		$('#imgLoading').fadeOut();
- }
+       
+ });
+}
+function onClickCurrent(e) {
+	$('#current').click(function(e){
+ 		$('#result').html('');
+		$('#imgLoading').fadeIn();
+		$('#error').fadeOut();
+		e.preventDefault();
+		makeAjaxReqCurrent()
+		$('#imgLoading').fadeOut();
+       
+ });
+}
 
- 
+function makeAjaxReqCurrent() {
+	// body...
+}
+
