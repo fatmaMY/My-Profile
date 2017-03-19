3cdfcd9620af88c9e51d909fe57cba2defe79f03
fatmaMY <fatima.mohammad736@gmail.com>
2017-03-20 00:33:31 +0300

    adding the nasa page and seting my requirements.

--- js/skills.js
+++ js/skills.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
- onClickMySkills();
+	onClickMySkills();
 });
 
 function onClickMySkills() {
 	$('#skillsList').click(function(e){
-		$(".list-group").fadeIn();
+		$(".list-group").fadeIn("slow");
 		e.preventDefault();
 		$('#respond').html('');
 		makeAjaxReqSkills();
@@ -12,18 +12,18 @@ function onClickMySkills() {
 }
 
 function makeAjaxReqSkills() {
-$.get("./skills.json",function(data){
+	$.get("./skills.json",function(data){
      //console.log(data)
      var html='';
      $.each(data.skills,function(i,res){
      	html+='<div class="list-group">'+
      	'<a href="#" class="list-group-item active">'+
-        '<h4 class="list-group-item-heading">'+res.name+'</h4>'+
-        '<p class="list-group-item-text">'+res.description+'</p>'+
-        '</a>'+
-        '</div>'
+     	'<h4 class="list-group-item-heading">'+res.name+'</h4>'+
+     	'<p class="list-group-item-text">'+res.description+'</p>'+
+     	'</a>'+
+     	'</div>'
 
-  });
-	$('#respond').append(html);
-	});
+     });
+     $('#respond').append(html);
+ });
 }
