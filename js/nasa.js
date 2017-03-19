89da24a4fd6ff323fc6a5b7780c99fd01fa01b4d
fatmaMY <fatima.mohammad736@gmail.com>
2017-03-20 01:12:24 +0300

    adding ajax request and making sure that I get respons.

--- js/nasa.js
+++ js/nasa.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
-
+$.ajax({ 
+	url: "https://api.nasa.gov/planetary/apod?api_key=LXZnFUPNcwZrPCIHgn2GDBDXSFhIGcxV4HYXGzta",
+    context: document.body,
+    success: function(data){
+           handleData();
+        }});
 
 });
+
+function handleData(data) {
+	// body...
+}
