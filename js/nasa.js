d1ab1382197b28fc1bf4558c1f741bc50f14df7f
fatmaMY <fatima.mohammad736@gmail.com>
2017-03-20 03:21:36 +0300

    adding handleData function and diplay the results on html

--- js/nasa.js
+++ js/nasa.js
@@ -1,13 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function(e){
 $.ajax({ 
 	url: "https://api.nasa.gov/planetary/apod?api_key=LXZnFUPNcwZrPCIHgn2GDBDXSFhIGcxV4HYXGzta",
     context: document.body,
     success: function(data){
-           handleData();
-        }});
+           handleData(data);
 
+        }});
 });
 
 function handleData(data) {
-	// body...
-}
+     console.log(data.url)
+   var html='<div class="row">'+
+     	'<div class="col-lg-12">'+
+          ' <div class="thumbnail">'+
+ 
+     	//'<a href="'+data.url+'" target="_blank" title="Watch"> <img class="img-thumbnail re-sized" src='+data.url+'></a>'+
+     	' <div class="caption">'+
+     	'<h3 id="date">'+data.date+'</h3>'+
+     	'<h3 id="explanation">'+data.title+'</h3>'+
+     	' <p>'+data.explanation+'</p>'+
+     	'</div>'+
+     	'</div>'+
+     	'</div>'+
+     	'</div>'
+
+			$('#video').attr('src', data.url);
+
+	     
+     $('#result').append(html);
+ }
